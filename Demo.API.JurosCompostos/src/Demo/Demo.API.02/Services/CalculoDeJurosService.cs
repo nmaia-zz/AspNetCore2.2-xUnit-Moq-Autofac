354824545dad6a1a32424cae78f97a60728c0af2
Natanael <gm.natanael@gmail.com>
2020-10-27 03:08:15 -0300

    Refatorando código. Removendo código desnecessário.

--- Demo.API.JurosCompostos/src/Demo/Demo.API.02/Services/CalculoDeJurosService.cs
+++ Demo.API.JurosCompostos/src/Demo/Demo.API.02/Services/CalculoDeJurosService.cs
@@ -7,13 +7,6 @@ namespace Demo.API._02.Services
     public class CalculoDeJurosService
         : ICalculoDeJuros
     {
-        private readonly IApi01Request _api01Request;
-
-        public CalculoDeJurosService(IApi01Request api01Request)
-        {
-            _api01Request = api01Request;
-        }
-
         public decimal CalculaJurosCompostos(decimal valorInicial, int tempo, decimal taxadejuros)
         {
             var valorFinal = Decimal.ToDouble(valorInicial) * Math.Pow(Decimal.ToDouble(1 + taxadejuros), tempo);
